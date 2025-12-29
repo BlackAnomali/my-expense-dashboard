fc520978f44155455193eff44ba1bb5320039657
BlackAnomali <ntaaa.creative@gmail.com>
2025-12-29 13:21:52 +0700

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    <NuxtPage />
+    <div v-if="expenseStore.isLoading" class="flex h-screen items-center justify-center">
+      <p class="text-lg animate-pulse">Memuat data pengeluaran...</p>
+    </div>
+    
+    <NuxtPage v-else />
   </div>
 </template>
+
+<script setup>
+import { useExpenseStore } from '~/stores/expenseStore'
+
+const expenseStore = useExpenseStore()
+
+// Perintah untuk mengambil data segera setelah aplikasi dimuat di browser
+onMounted(() => {
+  expenseStore.fetchData()
+})
+</script>

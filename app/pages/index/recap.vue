d72989c196a26548339cd2b46b88e38b88e8656c
BlackAnomali <ntaaa.creative@gmail.com>
2025-12-29 13:51:55 +0700

    Fix import path for expenseStore in recap.vue

--- app/pages/index/recap.vue
+++ app/pages/index/recap.vue
@@ -37,7 +37,7 @@
 import { onMounted, computed } from "vue";
 import DataTable from "@/components/DataTable.vue";
 import ExpenseFilters from "@/components/ExpenseFilters.vue";
-import { useExpenseStore } from "@/stores/expenseStore";
+import { useExpenseStore } from "@/store/expenseStore";
 
 const expenseStore = useExpenseStore();
 
